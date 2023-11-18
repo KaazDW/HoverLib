212ae95d32f098e6db238f534ef19a5da3283b91
/kaaz <92046563+KaazDW@users.noreply.github.com>
2023-11-18 11:51:20 +0100

    [dev] style import from js

--- tooltiper.js
+++ tooltiper.js
@@ -1,10 +1,17 @@
+
+const styleSheet = document.createElement('link');
+styleSheet.rel = 'stylesheet';
+styleSheet.type = 'text/css';
+styleSheet.href = 'https://raw.githubusercontent.com/KaazDW/Tooltiper/v1/tooltiper.css'; 
+document.head.appendChild(styleSheet);
+
 document.addEventListener('DOMContentLoaded', function () {
-    var labels = document.querySelectorAll('.tooltip-container');
+    let labels = document.querySelectorAll('.tooltip-container');
 
     labels.forEach(function (label) {
-        var tooltipText = label.getAttribute('data-tooltip');
+        let tooltipText = label.getAttribute('data-tooltip');
 
-        var tooltip = document.createElement('div');
+        let tooltip = document.createElement('div');
         tooltip.className = 'tooltip';
         tooltip.textContent = tooltipText;
 
